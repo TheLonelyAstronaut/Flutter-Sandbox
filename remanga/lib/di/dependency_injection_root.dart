54240abf8cc660f69022f12769741fa8e7702891
孤独な 宇宙飛行士 <vadim1230981@gmail.com>
2021-10-10 01:39:33 +0300

    app rearchitecture
    refactoring app architecture, now multifeature + clean architecture. App module -> Features modules -> Core module. Presentation -> Domain -> Data. New app navigation approach: v2 -> fluro, but we could change navigation approach in AppNavigator class.

--- remanga/lib/di/dependency_injection_root.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get_it/get_it.dart';
-import 'package:injectable/injectable.dart';
-
-import 'dependency_injection_root.config.dart';
-
-final instance = GetIt.instance;
-
-@InjectableInit()
-void configureDependencies() => $initGetIt(instance);
